753b52b661fa5c24fc00ac640858b7365fbd4ec3
imac1 <Februarie1977>
2019-05-26 08:48:14 +0300

    added alert and update list functionality / display updated list

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["go fishing", "buy coffee", "call brianna"]
+    var itemArray = ["go fishing", "buy coffee", "call brianna"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,5 +48,30 @@ class ToDoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK: Add new items on the Todo list
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what happens when the user clicks the Add Item Button on the UIAlert
+            print("Success!")
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            print(alertTextField.text as Any)
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
