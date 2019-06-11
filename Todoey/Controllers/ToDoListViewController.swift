0f3a5283cb91c9fc9767971cbe7fa4a8bbd775f4
imac1 <Februarie1977>
2019-06-11 10:13:11 +0300

    UISearchBar implementation with Core Data query

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -24,8 +24,10 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
 
-
+// let request : NSFetchRequest<Item> = Item.fetchRequest()
         loadItems()
         
          print(FileManager.default.urls(for: .documentDirectory,
@@ -125,18 +127,43 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
-        
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
         
         do {
             itemArray =  try context.fetch(request)
         }catch {
             print("Error fetching data from context \(error)")
         }
-
+tableView.reloadData()
     }
+
     
 }
 
+// MARK - search bar methods
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate  = NSPredicate(format: "title CONTAINS [cd] %@", searchBar.text!)
+        
+       
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+       loadItems(with: request)
+//
+//
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                 searchBar.resignFirstResponder()
+            }
+            
+           
+        }
+        
+ }
+}
